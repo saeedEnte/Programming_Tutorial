cb3068bfc29927389a94b51641382478320cd825
saeed <entezary.saeed@gmail.com>
2017-04-12 15:04:09 +0200

    I have used the formula to convert the score! it works for all grading systems all around the world

--- Structures/src/AssignGrade.java
+++ Structures/src/AssignGrade.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class AssignGrade {
 	
 	public static void main(String[] args) {
+		
+		System.out.println("Please input a grade:");
 		Scanner scan = new Scanner(System.in);
+		double inpGrade = scan.nextInt();
+		double germanGrade = ((100-inpGrade)/50)*3+1.0;
+		double deutschScore = germanGrade *10;
+		System.out.println("the score in German system is:"+java.lang.Math.floor(deutschScore)/10);
 		
 		//todo: when executing the program you will be ask to write a score.
 		//The program converts the score then into a grade and prints that grade (1.0, 1.3, 1.7, ...)
