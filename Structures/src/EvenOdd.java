deee022fb53628105caad73b58b60cd071987131
saeed <entezary.saeed@gmail.com>
2017-04-19 13:52:48 +0200

    this will show odd numbers

--- Structures/src/EvenOdd.java
+++ Structures/src/EvenOdd.java
@@ -5,8 +5,20 @@ import java.util.Scanner;
 public class EvenOdd {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Please input a number:");
+		Scanner scan = new Scanner(System.in);
+		int desNum = scan.nextInt();
+		
+		for (int i = 0; i < desNum; i++) {
+			if (i==desNum) {
+				break;
+			} else {
+				if (i%2 != 0) {
+					System.out.println(i);
+				}
+			}
+		}
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
